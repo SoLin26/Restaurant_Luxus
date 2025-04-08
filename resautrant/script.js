ee0a955e8c13fa94ef68559ce0fe95d389ed3b1d
So Li <sonja.linhard@dci-student.org>
2025-04-08 09:35:28 +0200

    f

--- resautrant/script.js
+++ resautrant/script.js
@@ -1,41 +1,61 @@
 // Beispielhafte Menü-Daten (simuliert)
-const menuData = [
-  {
-    name: "Foie Gras Maison",
-    description: "Serviert mit Feigenkonfitüre",
-    price: "28€",
-    image: "../images/foie_gras.jpg"
-  },
-  {
-    name: "Filet de Bœuf Rossini",
-    description: "Mit Trüffeln und Rotweinsauce",
-    price: "42€",
-    image: "../images/filet_de_boeuf_rossini-1.jpeg"
-  },
-  {
-    name: "Homard Thermidor",
-    description: "Im Ofen gratiniert mit cremiger Sauce",
-    price: "38€",
-    image: "../images/homard_thermidor.jpg"
-  }
-];
+const menuData = {
+  vorspeise: [
+    {
+      name: "Foie Gras Maison",
+      description: "Serviert mit Feigenkonfitüre",
+      price: "28€",
+      image: "../images/foie_gras.jpg"
+    }
+  ],
+  hauptspeise: [
+    {
+      name: "Filet de Bœuf Rossini",
+      description: "Mit Trüffeln und Rotweinsauce",
+      price: "42€",
+      image: "../images/filet_de_boeuf_rossini-1.jpeg"
+    }
+  ],
+  nachspeise: [
+    {
+      name: "Homard Thermidor",
+      description: "Im Ofen gratiniert mit cremiger Sauce",
+      price: "38€",
+      image: "../images/homard_thermidor.jpg"
+    }
+  ]
+};
 
 window.addEventListener("DOMContentLoaded", () => {
-  const menuContainer = document.getElementById("menu-items");
+  // Funktion zum Anzeigen des Menüs
+  window.zeigeMenü = function(menu) {
+    // Alle Menüelemente ausblenden
+    const items = document.querySelectorAll('.menu-item');
+    items.forEach(item => {
+      item.style.display = 'none';
+      item.innerHTML = ''; // Inhalt zurücksetzen
+    });
 
-  menuData.forEach(item => {
-    const div = document.createElement("div");
-    div.classList.add("menu-item");
-    div.innerHTML = `
-      <div class="img-wrapper">
-        <img src="${item.image}" alt="${item.name}" class="menu-img" />
-      </div>
-      <h3>${item.name}</h3>
-      <p>${item.description}</p>
-      <strong>${item.price}</strong>
-    `;
-    menuContainer.appendChild(div);
-  });
+    // Das ausgewählte Menüelement anzeigen
+    const selectedItems = menuData[menu];
+    const selectedItemDiv = document.getElementById(menu);
+    if (selectedItems && selectedItemDiv) {
+      selectedItems.forEach(item => {
+        const div = document.createElement("div");
+        div.classList.add("menu-item");
+        div.innerHTML = `
+          <div class="img-wrapper">
+            <img src="${item.image}" alt="${item.name}" class="menu-img" />
+          </div>
+          <h3>${item.name}</h3>
+          <p>${item.description}</p>
+          <strong>${item.price}</strong>
+        `;
+        selectedItemDiv.appendChild(div);
+      });
+      selectedItemDiv.style.display = 'block'; // Zeige das ausgewählte Menüelement
+    }
+  };
 
   const form = document.getElementById("res-form");
   if (form) {
